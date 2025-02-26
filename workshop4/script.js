2d74a150c32fce91b421e964164565700f5b0c54
Nana Sarpong <ns8363@g.rit.edu>
2025-02-26 16:17:13 -0500

    new pos

--- workshop4/script.js
+++ workshop4/script.js
@@ -13,7 +13,7 @@ const loadPlaces = () => {
         const entity = document.createElement("a-entity");
 
         entity.setAttribute("gps-entity-place", `latitude: ${place.latitude}; longitude: ${place.longitude}`);
-        entity.setAttribute("geometry", "primitive: sphere; radius: 1");
+        entity.setAttribute("geometry", "primitive: sphere; radius: 0.5");
         entity.setAttribute("material", "color: blue");
 
         const text = document.createElement("a-text");
